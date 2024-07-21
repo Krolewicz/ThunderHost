ba692b97f865f390bd762fd42aba32a1089aab40
Królewicz <118207873+Krolewicz@users.noreply.github.com>
2024-07-21 16:23:53 +0200

    dowland

---
+++ includes/footer.php
@@ -0,0 +1,70 @@
+	<section class="footer_top mt-5">
+        <div class="container-fluid container-custom">
+            <div class="row d-flex justify-content-between">
+                <div class="col-md-3 d-flex align-items-center">
+                    <div class="d-flex justify-content-between">
+                        <div class="ms-2 d-flex align-items-center">
+                            <h4 class="mb-0">NEW<span>Host</span></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-center">
+                    <div class="asds">
+                        <h1 class="my-0">High Performance Hosting Services For Less</h1>
+                    </div>
+                </div>
+                <div class="col-md-2 d-flex align-items-center">
+                    <div class="social">
+                        <ul>
+                            <li><a href=""><i class="fab fa-discord"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer_bottom py-5">
+    	<div class="container-fluid container-custom">
+    		<div class="row">
+    			<div class="col-md-3">
+    				<h3>Hosting</h3>
+    				<ul>
+    					<li><a href="">Game Servers</a></li>
+    					<li><a href="">Essential Dedicated Servers</a></li>
+    					<li><a href="">Premium Dedicated Servers</a></li>
+    					<li><a href="">Web Hosting</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<h3>Support</h3>
+    				<ul>
+    					<li><a href="">Contact Us</a></li>
+    					<li><a href="">Discord Support</a></li>
+    					<li><a href="">Knowledge Base</a></li>
+    					<li><a href="">Client Area</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<h3>Company</h3>
+    				<ul>
+    					<li><a href="">Acceptable Usage Policy</a></li>
+    					<li><a href="">Privacy Policy</a></li>
+    					<li><a href="">Terms of Service</a></li>
+    				</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<h3>NEWHost</h3>
+    				<p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit.</p>
+    				<p class="text-muted">&copy; NEWHost - All rights reserved 2022<br />
+					Report abuse @ <a href="mailto:abuse@newhost.com">abuse@newhost.com</a></p>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+	<script src="assets/js/jquery.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+	<script src="assets/js/main.js"></script>
---
+++ includes/header.php
@@ -0,0 +1,195 @@
+
+	<!-- CountDown Date -->
+	<section class="countdown">
+		<div class="container-fluid container-custom">
+			<div class="row d-flex justify-content-between">
+				<div class="col-lg-4 d-flex align-items-center">
+					<div class="d-flex align-items-center me-auto countdown-left">
+						<span class="countdown-l">Take 15% off your first payment! </span>
+					</div>
+				</div>
+				<div class="col-lg-4 d-flex align-items-center">
+					<div class="d-flex align-items-center mx-auto countdown-center">
+						<div class="text-center">
+							<h6 class="countdown-i my-0 mx-2"><span id="countdown-tld" class="me-1"></span>Days</h6>
+						</div>
+						<div class="text-center">
+							<h6 class="countdown-i my-0 mx-2"><span id="countdown-tlh" class="me-1"></span>Hours</h6>
+						</div>
+						<div class="text-center">
+							<h6 class="countdown-i my-0 mx-2"><span id="countdown-tlm" class="me-1"></span>Minutes</h6>
+						</div>
+						<div class="text-center">
+							<h6 class="countdown-i my-0 mx-2"><span id="countdown-tls" class="me-1"></span>Seconds</h6>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 d-flex justify-content-between">
+					<div class="d-flex align-items-center ms-auto countdown-right">
+						<div class="countdown-r" id="copy"><i class="fas fa-paperclip"></i> <span> Welcome15</span></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-dark">
+  		<div class="container-fluid container-custom">
+
+  			<!-- Navbar Logo -->
+		    <a class="navbar-brand" href="index">
+		    	<div class="d-flex justify-content-between">
+		    		<!--
+				    <div class="flex-shrink-0">
+				        <img src="assets/img/pALzFEQ.png" alt="Logo">
+				    </div>
+					-->
+				    <div class="ms-2 d-flex align-items-center">
+				    	<h4 class="mb-0">NEW<span>Host</span></h4>
+				    </div>
+				</div>
+		    </a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+		      	<span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarContent">
+
+		    	<!-- Navbar Links -->
+		      	<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+			        <li class="nav-item">
+			          	<a class="nav-link" href="index">Home</a>
+			        </li>
+			        <li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Game Servers</a>
+						<div class="dropdown-menu dropdown-large dropdown-menu-end fade-up">
+							<div class="row g-3 py-3 px-3">
+								<div class="col-6">
+									<ul class="list-unstyled">
+										<li><a href="ark-se"><img src="assets/img/game-icons/game-ark.png"> ARK:SE Hosting</a></li>
+										<li><a href="rust"><img src="assets/img/game-icons/game-rust.png"> RUST Hosting</a></li>
+										<li><a href="minecraft"><img src="assets/img/game-icons/game-minecraft.png"> Minecraft Hosting</a></li>
+									</ul>
+								</div>
+								<div class="col-6">
+									<ul class="list-unstyled">
+										<li><a href="csgo"><img src="assets/img/game-icons/game-csgo.png"> CS:GO Hosting</a></li>
+										<li><a href="garrysmod"><img src="assets/img/game-icons/game-garrysmod.png"> Garry's Mod Hosting</a></li>
+										<li><a href="conan"><img src="assets/img/game-icons/game-conan.png"> Conan Exiles Hosting</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="dropdown-footer py-2 text-center">
+								<a href="game-servers.php">View All</a>
+							</div>
+						</div>
+					</li>
+			        <li class="nav-item dropdown">
+			          	<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			            	VDS Game
+			          	</a>
+			          	<ul class="dropdown-menu dropdown-menu-end fade-up">
+			           		<li><a class="dropdown-item" href="">UK VPS</a></li>
+			           		<li><a class="dropdown-item" href="">Dallas VPS</a></li>
+			           		<li><a class="dropdown-item" href="">German VPS</a></li>
+			          	</ul>
+			        </li>
+			        <li class="nav-item dropdown">
+			          	<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			            	Dedicated
+			          	</a>
+			          	<ul class="dropdown-menu dropdown-menu-end fade-up">
+			           		<li><a class="dropdown-item" href="">UK</a></li>
+			           		<li><a class="dropdown-item" href="">Germany</a></li>
+			           		<li><a class="dropdown-item" href="">Dallas</a></li>
+			          	</ul>
+			        </li>
+			        <!--
+			        <li class="nav-item dropdown">
+			          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			            Dedicated
+			          </a>
+			          <ul class="dropdown-menu dropdown-menu-end fade-up">
+			            	<li>
+			            		<a class="dropdown-item" href="web-hosting">
+			            			<div class="d-flex align-items-center">
+			            				<div class="flex-shrink-0">
+				            				<img src="assets/img/web.svg" width="34" height="34">
+				            			</div>
+				            			<div class="ms-2 d-flex justify-content-between">
+				            				<div>
+					            				Web Hosting
+					            				<span class="d-block">Website Hosting</span>
+					            			</div>
+				            			</div>
+				            		</div>
+			            		</a>
+			            	</li>
+			            	<li>
+			            		<a class="dropdown-item" href="reseller-hosting">
+			            			<div class="d-flex align-items-center">
+			            				<div class="flex-shrink-0">
+				            				<img src="assets/img/reseller.svg" width="34" height="34">
+				            			</div>
+				            			<div class="ms-2 d-flex justify-content-between">
+				            				<div>
+					            				Reseller Hosting
+					            				<span class="d-block">Start your business</span>
+					            			</div>
+				            			</div>
+				            		</div>
+			            		</a>
+			            	</li>
+			          </ul>
+			        </li>-->
+			        <li class="nav-item">
+			          	<a class="nav-link" href="#">Web</a>
+			        </li>
+			        <li class="nav-item dropdown">
+			          	<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			            	Company
+			          	</a>
+			         	<ul class="dropdown-menu dropdown-menu-end fade-up">
+			            	<li><a class="dropdown-item" href="about-us">About us</a></li>
+			            	<li><a class="dropdown-item" href="partners">Partners</a></li>
+			            	<li><a class="dropdown-item" href="legal">Legal</a></li>
+			            	<li><a class="dropdown-item" href="">Knowlegebase</a></li>
+			         	</ul>
+			        </li>
+			        <li class="text-center">
+			        	<div class="dropdown">
+  							<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownLogin" data-bs-toggle="dropdown" aria-expanded="false">
+    							Login
+ 	 						</button>
+							<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownLogin">
+							    <li>
+							    	<a class="dropdown-item" href="#">
+							    		Client Area
+					            		<span class="d-block">Billing & Support</span>
+								    </a>
+								</li>
+							    <li>
+							    	<a class="dropdown-item" href="#">
+							    		Game Panel
+					            		<span class="d-block">Game Management</span>
+							    	</a>
+							    </li>
+							    <li>
+							    	<a class="dropdown-item" href="#">
+							    		cPanel
+					            		<span class="d-block">Website Management</span>
+							    	</a>
+							    </li>
+							    <li>
+							    	<a class="dropdown-item" href="#">
+							    		Bot Panel
+					            		<span class="d-block">Bot Control Management</span>
+							    	</a>
+							    </li>
+							</ul>
+						</div>
+			        </li>
+		      	</ul>
+		    </div>
+  		</div>
+	</nav>
